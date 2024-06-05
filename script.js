2b061d4c054072c65c2ad491bbfaf6c88290880f
Ygor Queiroz <ygor.silva@eaportal.org>
2024-06-05 19:35:38 +0000

    função escolher

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 import { trocarTema, verificarTema } from "./helpers/tema-helper.js"
+
 const botaoTema = document.querySelector(".tema button")
 const body = document.querySelector("body")
+
 botaoTema.addEventListener("click", () => {
     trocarTema(body, botaoTema)
 })
