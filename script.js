f9386082cac9aa9d9b0f626c7d748abf1e824279
Ygor Queiroz <ygor.silva@eaportal.org>
2024-06-05 18:45:22 +0000

    criação do helper

--- script.js
+++ script.js
@@ -1,32 +1,7 @@
-let tema = "claro"
+import { trocarTema, verificarTema } from "./helpers/tema-helper.js"
 
-const botaoTema = document.querySelector(".tema button")
-botaoTema.addEventListener("click", trocarTema)
+botaoTema.addEventListener("click", () => {
+    trocarTema(body, botaoTema)
+})
 
-const body = document.querySelector("body")
-
-function trocarTema() {
-    if (localStorage.getItem("tema")) {
-        tema = localStorage.getItem("tema")
-    }
-
-    if (tema === "claro") {
-        body.classList.add("escuro")
-        localStorage.setItem("tema", "escuro")
-    } else {
-        body.classList.remove("escuro")
-        localStorage.setItem("tema", "claro")
-    }
-}
-
-function verificarTema() {
-    if (localStorage.getItem("tema")) {
-        tema = localStorage.getItem("tema")
-    }
-
-    if (tema === "escuro") {
-        body.classList.add("escuro")
-    }
-}
-
-verificarTema()
+verificarTema(body, botaoTema)
