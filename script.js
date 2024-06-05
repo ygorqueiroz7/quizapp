180e08077b75c682d6427a0cffd4c07a4d48474e
Ygor Queiroz <ygor.silva@eaportal.org>
2024-06-05 19:20:16 +0000

    js adicionado na pagina

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
 import { trocarTema, verificarTema } from "./helpers/tema-helper.js"
-
+const botaoTema = document.querySelector(".tema button")
+const body = document.querySelector("body")
 botaoTema.addEventListener("click", () => {
     trocarTema(body, botaoTema)
 })
 
 verificarTema(body, botaoTema)
+
+const botoesAssunto = document.querySelectorAll(".assuntos button")
+botoesAssunto.forEach(botao => {
+    botao.addEventListener("click", selecionarAssunto)
+})
+
+function selecionarAssunto(evento) {
+    const assunto = evento.target.innerText
+    localStorage.setItem("assunto", assunto)
+    window.location.href = "./pages/quiz/quiz.html"
+}
