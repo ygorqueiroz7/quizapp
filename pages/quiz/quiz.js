5573282c19ad9bf8c55d2779b53627185ebffcc1
Ygor Queiroz <ygor.silva@eaportal.org>
2024-06-06 20:16:51 +0000

    guardar id e resposta

--- pages/quiz/quiz.js
+++ pages/quiz/quiz.js
@@ -1,100 +1,107 @@
-import { verificarTema, trocarTema } from "../../helpers/tema-helper.js";
+import { verificarTema, trocarTema } from "../../helpers/tema-helpers.js";
 const botaoTema = document.querySelector(".tema button")
 const body = document.querySelector("body")
 const assunto = localStorage.getItem("assunto")
-let quiz = {}
+let quiz = []
 let pontos = 0
-let pergunta = 2
+let pergunta = 1
 let resposta = ""
 let idInputResposta = ""
+
 
 botaoTema.addEventListener("click", () => {
     trocarTema(body, botaoTema)
 })
 verificarTema(body, botaoTema)
-function alterarAssunto () {
+console.log(assunto.toLowerCase())
+function alterarAssunto() {
     const divIcone = document.querySelector(".assunto_icone")
-    const iconeImg = document.querySelector(".assunto_icone img") 
+    const iconeImg = document.querySelector(".assunto_icone img")
     const assuntoTitulo = document.querySelector(".assunto h1")
     divIcone.classList.add(assunto.toLowerCase())
     iconeImg.setAttribute("src", `../../assets/images/icon-${assunto.toLowerCase()}.svg`)
-    iconeImg.setAttribute("alt", `ícone de ${assunto}`)
+    iconeImg.setAttribute("alt", `Icone de ${assunto}`)
     assuntoTitulo.innerText = assunto
 }
-
-async function buscarPerguntas () {
+async function buscarPerguntas() {
     const urlDados = "../../data.json"
     await fetch(urlDados).then(resposta => resposta.json()).then(dados => {
-        dados.quizzes.forEach (dado => {
+        dados.quizzes.forEach(dado => {
             if (dado.title === assunto) {
                 quiz = dado
             }
         })
     })
 }
-    function montarPergunta() {
-        const main = document.querySelector("main")
-        main.innerHTML = `
-            <section class="pergunta">
-                <div>
+function montarPergunta() {
+    const main = document.querySelector("main")
+    main.innerHTML = `
+        <section class="pergunta">
+            <div>
                     <p>Questão ${pergunta} de 10</p>
                     <h2>${alterarSinais(quiz.questions[pergunta-1].question)}</h2>
-                </div>
+            </div>
                 <div class="barra_progresso">
-                    <div style="width: ${pergunta * 10}%"></div>
+                    <div style="width: ${pergunta*10}%"></div>
                 </div>
             </section>
             <section class="alternativas">
                 <form action="">
-                    <label for="alternativa_a">
-                    <input type="radio" id="alternativa_a" name="alternativa" value="${alterarSinais(quiz.questions[pergunta-1].options[0])}">
-                     
-                    <div>
-                    <span>A</span>
-                    ${alterarSinais(quiz.questions[pergunta-1].options[0])}
-                </div>
-            </label>
-            <label for="alternativa_b">
-            <div>
-            <span>B</span>
-            ${alterarSinais(quiz.questions[pergunta-1].options[1])}
-        </div>
-    </label>
-    <label for="alternativa_c">
-    <div>
+                    <label for="alternativa_a" >
+                      <input type="radio" id="alternativa_a"  name="alternativa" value="${alterarSinais(quiz.questions[pergunta-1].options[0])}">
+                        <div>
+                            <span>A</span>
+                            ${alterarSinais(quiz.questions[pergunta-1].options[0])}
+                        </div>
+                    </label>
+                    <label for="alternativa_b">
+                        <input type="radio" id="alternativa_b" name="alternativa" value=" ${alterarSinais(quiz.questions[pergunta-1].options[1])}">
+                        <div>
+                            <span>B</span>
+                            ${alterarSinais(quiz.questions[pergunta-1].options[1])}
+                        </div>
+                    </label>
+                    <label for="alternativa_c">
+                     <input type="radio" id="alternativa_c" name="alternativa" value="${alterarSinais(quiz.questions[pergunta-1].options[2])}">
+                        <div>
                             <span>C</span>
                             ${alterarSinais(quiz.questions[pergunta-1].options[2])}
                         </div>
                     </label>
                     <label for="alternativa_d">
-                    <div>
-                    <span>D</span>
-                    ${alterarSinais(quiz.questions[pergunta-1].options[3])}
-                </div>
-            </label>
-        </form>
-        <button>Enviar</button>
-    </section>
-`
+                      <input type="radio" id="alternativa_d" name="alternativa" value=" ${alterarSinais(quiz.questions[pergunta-1].options[3])}">
+                        <div>
+                            <span>D</span>
+                            ${alterarSinais(quiz.questions[pergunta-1].options[3])}
+                        </div>
+                    </label>
+                </form>
+                <button>Enviar</button>
+        </section>
+        
+    
+    
+    `
 }
-function alterarSinais(texto) {
-return texto.replace(/</g, "&lt;").replace(/>/g, "&gt;")
+function alterarSinais(texto){
+    return texto.replace(/</g, "&lt;").replace(/>/g, "&gt;")
 }
 
-function guardarResposta(evento) {
-resposta = evento.target.value
-idInputResposta = evento.target.id
+function guardarResposta(evento){
+    resposta = evento.target.value
+    idInputResposta = evento.target.id
 }
 
-async function iniciar() {
-alterarAssunto()
-await buscarPerguntas()
-montarPergunta()
 
-const inputsResposta = document.querySelectorAll(".alternativas input") 
-inputsResposta.forEach (input => {
-    input.addEventListener("click", guardarResposta)
-})
+async function iniciar(){
+    alterarAssunto()
+    await buscarPerguntas()
+    montarPergunta()
+
+    const inputsResposta = document.querySelectorAll(".alternativas input")
+    inputsResposta.forEach(input => {
+        input.addEventListener("click", guardarResposta)
+    })
 }
 
 iniciar()
